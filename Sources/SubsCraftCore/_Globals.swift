bebf549b3546766142ca0c1d5425e5b81701c773
dDomovoj <dmitryduleba@gmail.com>
2022-06-23 20:56:41 +0200

    [add] initial code

---
+++ Sources/SubsCraftCore/_Globals.swift
@@ -0,0 +1,11 @@
+//
+//  _Globals.swift
+//
+//  Created by dDomovoj on 6/22/22.
+//
+
+import Foundation
+import UIKit
+
+typealias VoidHandler = () -> Swift.Void
+typealias BoolHandler = (Swift.Bool) -> Swift.Void
