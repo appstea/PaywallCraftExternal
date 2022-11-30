4d114b3280e3f37aa03c9871eaced3958c9f4030
dDomovoj <dmitryduleba@gmail.com>
2022-11-30 21:21:08 +0100

    [wip] configurable elements appearance

--- Sources/PaywallCraftUI/BannerView~Default.swift
+++ Sources/PaywallCraftUI/BannerView~Default.swift
@@ -26,8 +26,14 @@ public extension UpsellView {
     }
 
     public struct ViewModel {
-      public var title = L10n.DefaultUpsell.title
-      public var titleColor = UIColor.black
+      
+      public var title = VM.Label(
+        VM.Text(L10n.DefaultUpsell.title)
+          .textColor(.black)
+          .dynamicFont(.systemFont(ofSize: Const.upgradeLabelFontSize, weight: .medium))
+      )
+        .numberOfLines(2)
+        .adjustsFontSize(.minScale(isPad ? 0.5 : 0.75))
 
       public var upgradeTitle = L10n.DefaultUpsell.Upgrade.title
       public var upgradeTextColor = UIColor.black
@@ -38,8 +44,7 @@ public extension UpsellView {
       public init() { }
 
       fileprivate func apply(to view: DefaultView) {
-        view.titleLabel.text = title
-        view.titleLabel.textColor = titleColor
+        view.titleLabel.apply(title)
 
         view.upgradeLabel.text = upgradeTitle
         view.upgradeLabel.textColor = upgradeTextColor
@@ -55,29 +60,15 @@ public extension UpsellView {
 
     // MARK: UI
 
-    fileprivate lazy var titleLabel: UIBase.Label = {
-      let result = UIBase.Label()
-      result.numberOfLines = 2
-      result.setDynamicFont(font: .systemFont(ofSize: Const.upgradeLabelFontSize, weight: .medium))
-      result.adjustsFontSizeToFitWidth = true
-      result.minimumScaleFactor = isPad ? 0.5 : 0.75
-      return result
-    }()
-    fileprivate let ctaButton: UIBase.View = {
-      let result = UIBase.View()
-      result.layer.cornerRadius = Const.upgradeViewHeight / 2
-      return result
-    }()
-    fileprivate lazy var upgradeLabel: UIBase.Label = {
-      let result = UIBase.Label()
-      result.setContentCompressionResistancePriority(.required, for: .horizontal)
-      result.dynamicFont = DynamicFont.medium(of: Const.upgradeLabelFontSize).maxSize(to: 22)
-      return result
-    }()
-    fileprivate lazy var iconView: UIBase.ImageView = {
-      let result = UIBase.ImageView()
-      return result
-    }()
+    fileprivate lazy var titleLabel = UIBase.Label()
+    fileprivate let ctaButton = UIBase.View {
+      $0.layer.cornerRadius = Const.upgradeViewHeight / 2
+    }
+    fileprivate lazy var upgradeLabel = UIBase.Label {
+      $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+      $0.dynamicFont = DynamicFont.medium(of: Const.upgradeLabelFontSize).maxSize(to: 22)
+    }
+    fileprivate lazy var iconView = UIBase.ImageView()
 
     // MARK: - Lifecycle
 
@@ -116,4 +107,129 @@ public extension UpsellView {
     }
 
   }
+}
+
+// MARK: - VM
+
+public protocol IViewModel { }
+public protocol IView {
+  
+  associatedtype ViewModel: IViewModel
+  
+  func apply(_ viewModel: ViewModel)
+  
+}
+
+//public extension IViewModel {
+//  
+//  var ctx: UIContext { .init() }
+//  
+//}
+
+public enum VM { }
+
+public extension VM {
+  
+  struct Text {
+    fileprivate enum Internal {
+      fileprivate struct StringText {
+        var string: String
+        var font: UIFont?
+        var textColor: UIColor?
+        var adjustsFontForContentSizeCategory = false
+        
+        init(_ string: String) { self.string = string }
+      }
+      case string(StringText)
+      case attributed(NSAttributedString)
+    }
+    fileprivate var _internal: Internal
+    
+    public init(_ string: String) { _internal = .string(.init(string)) }
+    public init(_ attributedString: NSAttributedString) { _internal = .attributed(attributedString) }
+    
+    public func textColor(_ v: UIColor) -> Self {
+      guard case .string(var s) = _internal else { return self }
+      
+      var c = self
+      s.textColor = v
+      c._internal = .string(s)
+      return c
+    }
+    
+    public func font(_ v: UIFont) -> Self {
+      guard case .string(var s) = _internal else { return self }
+      
+      var c = self
+      s.font = v
+      c._internal = .string(s)
+      return c
+    }
+    
+    public func dynamicFont(_ v: UIFont, maximumPointSize: CGFloat? = nil) -> Self {
+      guard case .string(var s) = _internal else { return self }
+      
+      var c = self
+      s.font = UIFontMetrics(forTextStyle: .body)
+        .scaledFont(for: v, maximumPointSize: maximumPointSize ?? .greatestFiniteMagnitude)
+      s.adjustsFontForContentSizeCategory = true
+      c._internal = .string(s)
+      return c
+    }
+    
+    public func dynamicFont(_ v: DynamicFont) -> Self {
+      guard case .string(var s) = _internal else { return self }
+      
+      var c = self
+      s.font = v.asFont()
+      s.adjustsFontForContentSizeCategory = true
+      c._internal = .string(s)
+      return c
+    }
+        
+  }
+  
+  struct Label: IViewModel {
+    var text: Text
+    var numberOfLines = 1
+    var adjustsFontSize = FontSizeAdjustment.none
+    
+    public init(_ text: Text) { self.text = text }
+    
+    public func numberOfLines(_ v: Int) -> Self { var c = self; c.numberOfLines = v; return c }
+    
+    public enum FontSizeAdjustment {
+      case none
+      case minScale(CGFloat)
+    }
+    public func adjustsFontSize(_ v: FontSizeAdjustment) -> Self { var c = self; c.adjustsFontSize = v; return c }
+  }
+}
+
+extension UIBase.Label: IView {
+  
+  public typealias ViewModel = VM.Label
+  
+  public func apply(_ viewModel: VM.Label) {
+    switch viewModel.text._internal {
+    case .string(let info):
+      text = info.string
+      font = info.font
+      textColor = info.textColor
+      adjustsFontForContentSizeCategory = info.adjustsFontForContentSizeCategory
+    case .attributed(let attributedText):
+      self.attributedText = attributedText
+    }
+    numberOfLines = viewModel.numberOfLines
+    
+    switch viewModel.adjustsFontSize {
+    case .none:
+      minimumScaleFactor = 0
+      adjustsFontSizeToFitWidth = false
+    case .minScale(let scale):
+      minimumScaleFactor = scale
+      adjustsFontSizeToFitWidth = true
+    }
+  }
+  
 }
